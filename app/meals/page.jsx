41c832580e698c16680deef0aff9aa8713c432b7
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-05 23:14:47 +0330

    add MealsPage, MealItem, and MealsGrid components with associated styles

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,9 +1,32 @@
-const MealsPage = () => {
-    return (
-        <div>
-            <h2>Meals</h2>
-        </div>
-    )
-}
+import Link from "next/link";
 
-export default MealsPage
+import MealsGrid from "@/components/meals/MealsGrid";
+
+import styles from "./page.module.css";
+
+const MealsPage = () => {
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>
+					Delicious meals, created{" "}
+					<span className={styles.highlight}>by you</span>
+				</h1>
+				<p>
+					Choose your favorite recipe and cook it yourself. It is easy
+					and fun!
+				</p>
+				<p className={styles.cta}>
+					<Link href={"/meals/share"}>
+						Share Your Favorite Recipe
+					</Link>
+				</p>
+			</header>
+			<main className={styles.main}>
+                <MealsGrid meals={[]} />
+            </main>
+		</>
+	);
+};
+
+export default MealsPage;
