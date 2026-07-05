3f76781db7d95f3539ec39e8756748d79d9a06b2
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-05 22:51:44 +0330

    refactor MainHeader component to use NavLink for navigation and clean up unused styles

--- components/mainHeader/MainHeader.jsx
+++ components/mainHeader/MainHeader.jsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import NavLink from "./NavLink";
+import MainHeaderBackground from "./MainHeaderBackground";
+
 import logoImg from "@/assets/logo.png";
 import styles from "./MainHeader.module.css";
-import MainHeaderBackground from "./MainHeaderBackground";
 
 const MainHeader = () => {
 	return (
@@ -21,10 +23,12 @@ const MainHeader = () => {
 				<nav className={styles.nav}>
 					<ul>
 						<li>
-							<Link href="/meals">Browse Meals</Link>
+							<NavLink href="/meals">Browse Meals</NavLink>
 						</li>
 						<li>
-							<Link href="/community">Foodies Community</Link>
+							<NavLink href="/community">
+								Foodies Community
+							</NavLink>
 						</li>
 					</ul>
 				</nav>
