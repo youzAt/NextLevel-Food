949608f6ed8f8c74c5d63c1ad18c0749d8ec1fbd
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-10 20:48:34 +0330

    add ShareMealPage component with form for sharing meals and associated styles

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,9 +1,61 @@
-const ShareMealsPage = () => {
-    return (
-        <div>
-            <h2>Share Meals</h2>
-        </div>
-    )
-}
+import ImagePicker from "@/components/meals/ImagePicker";
+import styles from "./page.module.css";
 
-export default ShareMealsPage
+export default function ShareMealPage() {
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>
+					Share your{" "}
+					<span className={styles.highlight}>favorite meal</span>
+				</h1>
+				<p>Or any other meal you feel needs sharing!</p>
+			</header>
+			<main className={styles.main}>
+				<form className={styles.form}>
+					<div className={styles.row}>
+						<p>
+							<label htmlFor="name">Your name</label>
+							<input type="text" id="name" name="name" required />
+						</p>
+						<p>
+							<label htmlFor="email">Your email</label>
+							<input
+								type="email"
+								id="email"
+								name="email"
+								required
+							/>
+						</p>
+					</div>
+					<p>
+						<label htmlFor="title">Title</label>
+						<input type="text" id="title" name="title" required />
+					</p>
+					<p>
+						<label htmlFor="summary">Short Summary</label>
+						<input
+							type="text"
+							id="summary"
+							name="summary"
+							required
+						/>
+					</p>
+					<p>
+						<label htmlFor="instructions">Instructions</label>
+						<textarea
+							id="instructions"
+							name="instructions"
+							rows="10"
+							required
+						></textarea>
+					</p>
+					<ImagePicker name={"image"} label={"image"} />
+					<p className={styles.actions}>
+						<button type="submit">Share Meal</button>
+					</p>
+				</form>
+			</main>
+		</>
+	);
+}
