893ebefcce807acce8b12b6902c43f3266e03399
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-10 20:04:37 +0330

    add meal detail page with image, title, and instructions; implement NotFound page for missing meals

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,10 +1,41 @@
-const MealDetailPage = ({ mealSlug }) => {
-    return (
-        <div>
-            <h2>Meal Details</h2>
-            <p>Slug: {mealSlug}</p> 
-        </div>
-    )
-}
+import Image from "next/image";
 
-export default MealDetailPage
+import { getMeal } from "@/lib/meals";
+
+import styles from "./page.module.css";
+import { notFound } from "next/navigation";
+
+const MealDetailPage = async ({ params }) => {
+	const { mealSlug } = await params;
+	const meal = getMeal(mealSlug);
+	if (!meal) notFound();
+	meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+	return (
+		<>
+			<header className={styles.header}>
+				<div className={styles.image}>
+					<Image src={meal.image} alt={meal.title} fill />
+				</div>
+				<div className={styles.headerText}>
+					<h1>{meal.title}</h1>
+					<p className={styles.creator}>
+						by{" "}
+						<a href={`mailto:${meal.creator_email}`}>
+							{meal.creator}
+						</a>
+					</p>
+					<p className={styles.summary}>{meal.summary}</p>
+				</div>
+			</header>
+			<main>
+				<p
+					className={styles.instructions}
+					dangerouslySetInnerHTML={{ __html: meal.instructions }}
+				></p>
+			</main>
+		</>
+	);
+};
+
+export default MealDetailPage;
---
+++ app/meals/not-found.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+	return (
+		<main className="not-found">
+			<h1>meal not found</h1>
+			<p>
+				Unfortunately, we could not find the reqeusted page or meal data!
+			</p>
+		</main>
+	);
+};
+
+export default NotFoundPage;
