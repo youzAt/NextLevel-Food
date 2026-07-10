b4aa248ded2a11144bbfed4b98a9d42185402de3
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-10 19:13:45 +0330

    add NotFound page to display a user-friendly message for missing resources

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+const NotFoundPage = () => {
+	return (
+		<main className="not-found">
+			<h1>not found</h1>
+			<p>
+				Unfortunately, we could not find the reqeusted page or resource!
+			</p>
+		</main>
+	);
+};
+
+export default NotFoundPage;
