47a4661224da3882c0552a9170864d246c0762ba
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-10 19:13:25 +0330

    add error page to handle meal data fetching errors

---
+++ app/meals/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+const error = () => {
+	return (
+		<main className="error">
+			<h1>An Error Occurred</h1>
+			<p>Faield to fetch meals data. Please try again.</p>
+		</main>
+	);
+};
+
+export default error;
