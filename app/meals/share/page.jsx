2b414860e652e26c5658d8ca3287d701a2cb50da
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-12 13:19:28 +0330

    refactor: update ShareMealPage to use formAction for submission and enhance shareMeal validation with error messaging

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,11 +1,15 @@
+"use client";
+import { useFormState } from "react-dom";
+
 import ImagePicker from "@/components/meals/ImagePicker";
+import MealsFormSubmit from "@/components/meals/MealsFormSubmit";
 
 import { shareMeal } from "@/lib/actions";
 
 import styles from "./page.module.css";
-import MealsFormSubmit from "@/components/meals/MealsFormSubmit";
 
 export default function ShareMealPage() {
+	const [state, formAction] = useFormState(shareMeal, { message: null });
 	return (
 		<>
 			<header className={styles.header}>
@@ -16,7 +20,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={styles.main}>
-				<form className={styles.form} action={shareMeal}>
+				<form className={styles.form} action={formAction}>
 					<div className={styles.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
@@ -55,8 +59,9 @@ export default function ShareMealPage() {
 						></textarea>
 					</p>
 					<ImagePicker name={"image"} label={"image"} />
+					{state.message && <p>{state.message}</p>}
 					<p className={styles.actions}>
-						<MealsFormSubmit/>
+						<MealsFormSubmit />
 					</p>
 				</form>
 			</main>
