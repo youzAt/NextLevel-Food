b78ebc83aef3c540ee92e9520c20c7491fa4be4a
Masoud Yousefi <masoudyousefi.1967@gmail.com>
2026-07-12 13:08:21 +0330

    add error handling component and refactor ShareMealPage to use MealsFormSubmit for form submission

---
+++ app/meals/share/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+const error = () => {
+	return (
+		<main className="error">
+			<h1>An Error Occurred</h1>
+			<p>Failed to share food. Please try again.</p>
+		</main>
+	);
+};
+
+export default error;
--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,5 +1,9 @@
 import ImagePicker from "@/components/meals/ImagePicker";
+
+import { shareMeal } from "@/lib/actions";
+
 import styles from "./page.module.css";
+import MealsFormSubmit from "@/components/meals/MealsFormSubmit";
 
 export default function ShareMealPage() {
 	return (
@@ -12,7 +16,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={styles.main}>
-				<form className={styles.form}>
+				<form className={styles.form} action={shareMeal}>
 					<div className={styles.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
@@ -52,7 +56,7 @@ export default function ShareMealPage() {
 					</p>
 					<ImagePicker name={"image"} label={"image"} />
 					<p className={styles.actions}>
-						<button type="submit">Share Meal</button>
+						<MealsFormSubmit/>
 					</p>
 				</form>
 			</main>
